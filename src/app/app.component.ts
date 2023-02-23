466da1eab3408d22b19c0a029ef2eb08cccfe9d2
VOJAKIS <valastan1@gmail.com>
2023-02-23 17:44:07 +0100

    Bonusova uloha 3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+
+interface Fact {
+  fact: string;
+  length: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'library-fe';
+export class AppComponent implements OnInit {
+
+  fact: Fact = {
+    fact: '',
+    length: 0
+  };
+
+  constructor(private http:HttpClient) {
+  }
+
+  refresh(): void {
+    const url = 'https://catfact.ninja/fact';
+    const req = this.http.get<Fact>(url);
+    req.subscribe((data: Fact) => { 
+      this.fact = data;
+      console.log(data);
+    });
+  }
+
+  ngOnInit(): void {
+    this.refresh();
+  }
 }
